b8da86a5e63f02f870acead11e115df9d2165eec
yangho1231 <yangho1231@gmail.com>
2017-02-27 01:12:34 -0700

    Added alternate solution

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ function digital_root(n) {
   return (n-1)%9 + 1;
 }
 digital_root(16);
+
+//Alternative Solution
+
+function digital_root(n) {
+  var total = 0;
+  if(n.toString().length == 1) {
+    var num = parseInt(n);
+    return num;
+  }
+  else {
+    var split = n.toString().split('');
+    for(var i = 0; i < split.length; i++) {
+      var int = parseInt(split[i]);
+      total += int;
+    }
+    return digital_root(total);
+  }
+}
